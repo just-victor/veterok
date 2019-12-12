ef9577d5e061edd9c1dad79c2c1cd9b5faeb7b9e
Victor Samilenko <samilenkov@gmail.com>
2019-12-12 18:37:15 +0200

    test rdeploy

--- src/main/java/net/wind/anemometr/controller/HelloController.java
+++ src/main/java/net/wind/anemometr/controller/HelloController.java
@@ -1,5 +1,6 @@
 package net.wind.anemometr.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping
     public String status() {
         return "I'm alive! 1";
     }
